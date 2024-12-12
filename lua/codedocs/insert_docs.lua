2f9b705a70af9f3d2412b512e10e1d789455b18b
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-12-12 17:40:10 -0500

    refactor(languages): Simplify language template definition
    Previously, a language's template was a single string, requiring
    escape sequences (e.g., "\n") for line breaks. It also needed to
    be split into individual lines at runtime before being inserted
    into the buffer.
    
    This change replaces the string with a table, where each entry
    represents a line. This makes the template easier to understand
    and modify, and eliminates the need for splitting the template at
    runtime.

--- lua/codedocs/insert_docs.lua
+++ lua/codedocs/insert_docs.lua
@@ -1,26 +1,26 @@
-local function insert_above(template_lines)
-	local current_line = vim.fn.line(".") - 1
-	vim.api.nvim_buf_set_lines(0, current_line, current_line, false, template_lines)
+local function insert_above(docstring)
+	local current_line_pos = vim.fn.line(".") - 1
+	vim.api.nvim_buf_set_lines(0, current_line_pos, current_line_pos, false, docstring)
+
 	vim.cmd("normal! kk")
 	vim.cmd("normal! $")
 	vim.cmd('startinsert')
 	vim.api.nvim_input('<Right>')
 end
 
-local function start_doc_insertion(lang_details, chosen_docstring)
-	local template_lines = vim.split(chosen_docstring, '\n')
+local function start_doc_insertion(lang_details, docstring)
 	local docstring_pos = lang_details["pos"]
 	if docstring_pos == "above" then
-		insert_above(template_lines)
+		insert_above(docstring)
 	end
 end
 
 local function insert_documentation(lang_templates)
 	local filetype = vim.bo.filetype
 	local lang_details = lang_templates[filetype]
-	local lang_docstring = lang_details["template"]
-	if lang_docstring then
-		start_doc_insertion(lang_details, lang_docstring)
+	local docstring = lang_details["template"]
+	if docstring then
+		start_doc_insertion(lang_details, docstring)
 	else
 		print("There are no defined documentation strings for " .. filetype .. " files")
 	end
