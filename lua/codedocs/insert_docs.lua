73b763b5fda802d6b10660e829512dd6775c4b9c
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-12-15 20:40:37 -0500

    feat(languages): Add "title_pos" setting to language templates
    This update introduces the "title_pos" setting, allowing users to specify
    the position of the docstring title within its structure. When a docstring
    is inserted, the cursor moves to the specified line, and insert mode is
    activated, enabling users to immediately type a title or description.
    This improves the workflow and overall user experience.

--- lua/codedocs/insert_docs.lua
+++ lua/codedocs/insert_docs.lua
@@ -1,10 +1,30 @@
+--- Moves the cursor to the docstring title's position and enters insert mode
+-- Adjusts the cursor based on the docstring's placement relative to the function 
+-- and positions it at the title location. Automatically starts insert mode for 
+-- immediate typing.
+-- @param docstring_length (number) Total number of lines in the inserted docstring
+-- @param direction (string) Direction of the docstring insertion ("above" or "below" the function)
+-- @param title_pos (number) Line offset of the title within the docstring structure
+local function move_cursor_to_title(docstring_length, direction, title_pos)
+	local current_line_pos = vim.api.nvim_win_get_cursor(0)[1] -- (1-based index)
+	local line_length = #vim.api.nvim_buf_get_lines(0, current_line_pos - 1, current_line_pos, false)[1]
+	local target_line_pos = current_line_pos - (docstring_length - title_pos) - 1 -- If direction is "above"
+
+	if direction == "below" then
+		target_line_pos = current_line_pos + title_pos
+	end
+	vim.api.nvim_win_set_cursor(0, {target_line_pos, line_length})
+	vim.cmd('startinsert')
+	vim.api.nvim_input('<Right>')
+end
+
 --- Inserts a documentation string into the buffer relative to a function declaration.
 -- This function places the given "final_docstring" either above or below a function
--- declaration located at the specified "current_line" in the buffer.
+-- declaration located at the specified "current_line_pos" in the buffer.
 -- @param final_docstring (string) The string to be inserted into the buffer.
 -- @param insertion_direction (string) Direction to insert the string relative to the function declaration.
--- @param current_line (number) The 1-based line number where the cursor is located.
-local function insert_into_buffer(final_docstring,  insertion_direction, current_line)
+-- @param current_line_pos (number) The 1-based line number where the cursor is located.
+local function insert_into_buffer(final_docstring,  insertion_direction, current_line_pos)
 	if insertion_direction ~= "above" and insertion_direction ~= "below" then
 		error('\n\nYou can only insert a documentation string "above" or "below" a function. ' ..
 			'You used: ' .. insertion_direction .. '\n\n' ..
@@ -15,9 +35,9 @@ local function insert_into_buffer(final_docstring,  insertion_direction, current
 	local insertion_line_pos = 0
 
 	if insertion_direction == "above" then
-		insertion_line_pos = current_line - 1
+		insertion_line_pos = current_line_pos - 1
 	elseif insertion_direction == "below" then
-		insertion_line_pos = current_line
+		insertion_line_pos = current_line_pos
 	end
 	vim.api.nvim_buf_set_lines(0, insertion_line_pos, insertion_line_pos, false, final_docstring)
 end
@@ -27,14 +47,16 @@ end
 -- signature, and inserts the docstring into the buffer.
 -- @param templates (table) A map of languages to docstring configurations:
 local function insert_documentation(templates)
-	local current_line = vim.api.nvim_win_get_cursor(0)[1] -- Get the current cursor line (1-based index)
-	local line_content = vim.api.nvim_buf_get_lines(0, current_line -1, current_line, false)[1]
+	local current_line_pos = vim.api.nvim_win_get_cursor(0)[1] -- Get the current cursor line (1-based index)
+	local line_content = vim.api.nvim_buf_get_lines(0, current_line_pos -1, current_line_pos, false)[1]
 	local filetype = vim.api.nvim_buf_get_option(0, "filetype")
 	local template = templates[filetype]
 	local struct = template["struct"]
 	if struct then
+		local direction = template["direction"]
 		local final_docstring = require("codedocs.lua.codedocs.param_parser").get_final_docstring(template, struct, line_content)
-		insert_into_buffer(final_docstring, template["direction"], current_line)
+		insert_into_buffer(final_docstring, direction, current_line_pos)
+		move_cursor_to_title(#final_docstring, direction, template["title_pos"], line_content)
 	else
 		print("There are no defined documentation strings for " .. filetype .. " files")
 	end
--- lua/codedocs/languages.lua
+++ lua/codedocs/languages.lua
@@ -3,16 +3,18 @@
 --- The available settings for each language are as follows:
 ---
 --- struct (table): The base structure of a docstring, represented as a table with three elements:
----   1. The first element is the opening line of the docstring.
----   2. The middle element is the string prefixed to every line within the docstring body.
----   3. The third element is the closing line of the docstring.
+---   1. The first element is the opening line of the docstring
+---   2. The middle element is the string prefixed to every line within the docstring body
+---   3. The third element is the closing line of the docstring
 ---
---- func (string): The keyword used in the language to declare a function.
+--- func (string): The keyword used in the language to declare a function
 ---
---- direction (string): Specifies where to place the docstring relative to the function declaration.
+--- direction (string): Specifies where to place the docstring relative to the function declaration
 ---   Valid values are:
----     - "above": Place the docstring before the function declaration.
----     - "below": Place the docstring after the function declaration.
+---     - "above": Place the docstring before the function declaration
+---     - "below": Place the docstring after the function declaration
+---
+--- title_pos (number): The line offset of the title within the docstring, relative to its start
 ---
 --- params_title (string): The title displayed before the parametres section in the docstring. 
 ---   Some languages or docstring styles include a title for the parametres (e.g., "Args:" in Python),
@@ -23,6 +25,8 @@
 ---   For instance, some languages (e.g., JavaScript) use a specific keyword like "@param" before each
 ---   parametre, while others list parametres without a keyword. For the latter case, set "param_keyword"
 ---   to an empty string ("").
+---
+--- param_indent (bool): Determines wether or not the parametres should be indented.
 
 
 return {
@@ -30,6 +34,7 @@ return {
 			struct = {'"""', "", '"""'},
 			func = "def",
 			direction = "below",
+			title_pos = 2,
 			params_title = "Args:",
 			param_keyword = "",
 			param_indent = true
@@ -38,6 +43,7 @@ return {
 			struct = {"/**", " * ", " */"},
 			func = "function",
 			direction = "above",
+			title_pos = 2,
 			params_title = "",
 			param_keyword = "@param {} ",
 			param_indent = false
