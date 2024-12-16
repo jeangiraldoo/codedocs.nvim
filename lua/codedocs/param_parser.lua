1be2989e3edbea7067b70aead81a48f12062ce58
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-12-16 12:32:58 -0500

    refactor: Simplify function parametres by unpacking values internally
    This update removes unnecessary function parametres by passing the
    template table directly and unpacking needed values within each function,
    instead of unpacking them in the caller. This reduces boilerplate code,
    improves readability, and minimizes unnecessary variables.

--- lua/codedocs/param_parser.lua
+++ lua/codedocs/param_parser.lua
@@ -48,36 +48,33 @@ end
 
 --- Inserts a leader and parameters into the docstring
 -- Adds a title (args_title) before the parameters (if applicable) and inserts the parameters into the docstring
--- @param docstring_struct (table) The original table representing the docstring structure
--- @param args_title (string) A string to insert before the parameters (only for some languages)
+-- @param template (table) Settings to configure the language's docstring
 -- @param params (table) A table of parameters to be inserted into the docstring
--- @param arg_keyword (string) A keyword to be inserted before each parameter (only for some languages)
 -- @return (table) A new table with the updated docstring content
-local function update_docstring_content(docstring_struct, args_title, params, arg_keyword)
-	local docstring_copy = copy_docstring(docstring_struct)
-	local line_start = docstring_struct[2]
+local function update_docstring_content(template, params)
+	local docstring_copy = copy_docstring(template["struct"])
+	local line_start = template["struct"][2]
 	table.insert(docstring_copy, 3, line_start)
-	if args_title ~= "" then
-		table.insert(params, 1, args_title)
+	if template["params_title"] ~= "" then
+		table.insert(params, 1, template["params_title"])
 	end
 	for i = 1, #params do
-		table.insert(docstring_copy, i + 3, line_start .. arg_keyword .. params[i])
+		table.insert(docstring_copy, i + 3, line_start .. template["param_keyword"] .. params[i])
 	end
 	return docstring_copy
 end
 
 --- Returns the parametres found in the function under the cursor
+-- @param template (table) Settings to configure the language's docstring.
 -- @param line (string) The line under the cursor containing the function signature
--- @param func_keyword (string) The keyword used to define functions in the associated language
--- @param indentation (bool) Controls whether parameters are indented.
 -- @return (table|nil) A table with each parametre as an element, or nil if no parametres are found
-local function get_parametres(line, func_keyword, indentation)
+local function get_parametres(template, line)
 	local indentation_string = "\t"
-	if not indentation then
+	if not template["param_indent"] then
 		indentation_string = ""
 	end
 
-	if line and string.match(line, func_keyword) then
+	if line and string.match(line, template["func"]) then
 		local params_string = string.match(line, "%((.-)%)")
 		local params = vim.split(params_string, ", ")
 
@@ -92,21 +89,18 @@ end
 --- Returns a docstring with content or an empty one.
 -- If parameters and/or title are detected, updates the docstring with them; otherwise, returns the original docstring.
 -- @param template (table) Settings to configure the language's docstring.
--- @param docstring_struct (table) The structure of the docstring.
 -- @param line (string) The line under the cursor containing the function signature.
 -- @return (table) The updated docstring or the original one.
-local function get_final_docstring(template, docstring_struct, line)
-	local func_keyword = template["func"]
-	local func_indentation_string = get_indentation_string(line)
-	local detected_params = get_parametres(line, func_keyword, template["param_indent"])
-	local final_docstring = {}
+local function get_final_docstring(template, line)
+	local detected_params = get_parametres(template, line)
 
+	local final_docstring = {}
 	if not detected_params then
-		final_docstring = docstring_struct
+		final_docstring = template["struct"]
 	else
-		final_docstring = update_docstring_content(docstring_struct, template["params_title"], detected_params, template["param_keyword"])
+		final_docstring = update_docstring_content(template, detected_params)
 	end
-	return get_indented_docstring(final_docstring, func_indentation_string)
+	return get_indented_docstring(final_docstring, get_indentation_string(line))
 end
 
 return {
