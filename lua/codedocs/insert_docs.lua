8cb4905807c79c38e527b18b3dd08390eecb14d9
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2025-01-07 12:18:50 -0500

    refactor: Introduce "get_node_data" to reduce repetition
    Previously, "get_supported_node" returned a valid node if supported,
    or "nil" if not. This node was then passed to "get_docs_settings" to
    obtain the "opts" and "style" for the docstring, and later to
    "get_docs", where the node type was validated again, leading to
    repetitive code.
    
    This change introduces "get_node_data", which consolidates
    node-related settings and the generated docstring in a single function.
    This refactoring reduces redundancy and provides a more scalable
    solution as support for new structures is added.

--- lua/codedocs/insert_docs.lua
+++ lua/codedocs/insert_docs.lua
@@ -35,57 +35,48 @@ local function is_node_a_function(node_type)
 	return (identifiers[node_type]) and true or false
 end
 
+local function get_node_data(opts, style, ts_utils, node, struct_name)
+	local docs_style = style[struct_name]
+	local docs_opts = opts[struct_name]
+	local docs_copy = copy_docstring(docs_style[docs_opts.struct.val])
+
+	local docs_builder = require("codedocs.docs_builder.init")
+	local docs, pos
+
+	if struct_name == "generic" then
+		docs = docs_copy
+		pos = vim.api.nvim_win_get_cursor(0)[1] - 1
+	else
+		docs = docs_builder[struct_name].get_docs(docs_opts, docs_style, node, ts_utils, docs_copy)
+		pos = node:range()
+	end
+	return docs_opts, docs_style, docs, pos, struct_name
+end
+
 --- Traverses upwards through parent nodes to find a node of a supported type
 -- @param node_at_cursor Node found at the cursor's position
--- @return A node of a supported type or nil
-local function get_supported_node(node_at_cursor)
+local function get_struct_data(opts, style, ts_utils, node_at_cursor)
 	if node_at_cursor == nil then
-		return nil
+		return get_node_data(opts, style, ts_utils, node_at_cursor, "generic")
 	end
 
 	if is_node_a_function(node_at_cursor:type()) then
-		return node_at_cursor
+		return get_node_data(opts, style, ts_utils, node_at_cursor, "func")
   	end
 
   	-- Traverse upwards through parent nodes to find a function or method declaration
   	while node_at_cursor do
     	if is_node_a_function(node_at_cursor:type()) then
-      		return node_at_cursor
+      		return get_node_data(opts, style, ts_utils, node_at_cursor, "func")
     	end
 
     -- If it's a module or another node, continue traversing upwards
     	node_at_cursor = node_at_cursor:parent()
   	end
 
-  	-- If no function or method node is found, return nil
-  	return nil
+  	return get_node_data(opts, style, ts_utils, node_at_cursor, "generic")
 end
 
-local function get_docs_settings(opts, style, node)
-	local docs_style, docs_opts, struct_name
-	if node == nil then
-		docs_style = style["generic"]
-	docs_opts = opts["generic"]
-		struct_name = "generic"
-	elseif is_node_a_function(node:type()) then
-		docs_style = style["func"]
-		docs_opts = opts["func"]
-		struct_name = "func"
-	end
-	return {docs_opts, docs_style, struct_name}
-end
-
-local function get_docs(opts, style, valid_node, ts_utils)
-	local docs_builder = require("codedocs.docs_builder.init")
-	local docs_copy = copy_docstring(style[opts.struct.val])
-	local docs
-	if valid_node == nil then
-		docs = style[opts.struct.val]
-	elseif is_node_a_function(valid_node:type()) then
-		docs = docs_builder.func.get_docs(opts, style, valid_node, ts_utils, docs_copy)
-	end
-	return docs
-end
 -- Function to convert tabs to spaces based on the tabstop value
 local function convert_tabs_to_spaces(indentation, tabstop)
     return indentation:gsub("\t", string.rep(" ", tabstop))
@@ -115,29 +106,20 @@ end
 
 local function insert_docs(opts, style)
 	local ts_utils = require'nvim-treesitter.ts_utils'
-	local valid_node = get_supported_node(ts_utils.get_node_at_cursor())
-	local docs_settings = get_docs_settings(opts, style, valid_node)
-	local docs_opts, docs_style, struct_name = docs_settings[1], docs_settings[2], docs_settings[3]
+	local node = ts_utils.get_node_at_cursor()
+	local docs_opts, docs_style, docs, pos, struct_name = get_struct_data(opts, style, ts_utils, node)
 	require("codedocs.style_validations").validate_style(docs_opts, docs_style, struct_name)
-	local docs = get_docs(docs_opts, docs_style, valid_node, ts_utils)
 
-	local node_pos
-	if valid_node == nil then
-		node_pos = vim.api.nvim_win_get_cursor(0)[1] - 1
-	else
-		node_pos = valid_node:range()
-	end
-
-	local line_content = vim.api.nvim_buf_get_lines(0, node_pos, node_pos + 1, false)[1]
+	local line_content = vim.api.nvim_buf_get_lines(0, pos, pos + 1, false)[1]
 
 	local direction = docs_style[docs_opts.direction.val]
     local tabstop = vim.api.nvim_buf_get_option(0, "tabstop")
 	local indent_spaces = get_line_indentation(line_content, tabstop)
 	docs = add_indent_to_docs(docs, indent_spaces, tabstop, direction)
-	local insert_pos = (direction) and node_pos or node_pos + 1
+	local insert_pos = (direction) and pos or pos + 1
 	vim.api.nvim_buf_set_text(0, insert_pos, 0, insert_pos, 0, {"", ""})
 	vim.api.nvim_buf_set_text(0, insert_pos, 0, insert_pos, 0, docs)
-	move_cursor_to_title(node_pos, direction, docs_style[docs_opts.title_pos.val])
+	move_cursor_to_title(pos, direction, docs_style[docs_opts.title_pos.val])
 end
 
 --- Inserts a docstring for the function under the cursor
