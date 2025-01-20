137bb4c9d90e312eb5eb3a99687ff324234987e8
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2025-01-19 19:02:15 -0500

    refactor(get_parsed_items): Extract identifier_pos logic
    This refactor extracts the identifier_pos-related logic into two
    new functions: get_parsed_item_first and get_parsed_item_last.
    
    The get_parsed_items function was previously cluttered with multiple
    if statements, making it difficult to follow and debug. The extracted
    functions improve readability, maintainability, and allow for easier
    debugging in the future.

--- lua/codedocs/node_parser/parser.lua
+++ lua/codedocs/node_parser/parser.lua
@@ -26,47 +26,59 @@ local function search_node_recursively(node, node_type, def_val)
 	end
 end
 
-local function parse_node_with_identifier_first(node, include_type, filetype, queries, identifier_pos)
+local function get_parsed_item_name_first(capture_name, items, current_item, node_text)
+	if capture_name == "item_name" then
+		if current_item.name ~= nil then
+			table.insert(items, current_item)
+			current_item = {}
+			current_item.name = node_text
+		else
+			current_item.name = node_text
+		end
+	elseif capture_name == "item_type" then
+		current_item.type = node_text
+		table.insert(items, current_item)
+		current_item = {}
+	end
+	return current_item
+end
+
+local function get_parsed_item_name_last(capture_name, items, current_item, node_text)
+	if capture_name == "item_name" then
+		current_item.name = node_text
+		table.insert(items, current_item)
+		current_item = {}
+	elseif capture_name == "item_type" then
+		current_item.type = node_text
+	end
+	return current_item
+end
+
+local function get_parsed_items(node, include_type, filetype, queries, identifier_pos)
 	include_type = true
-	local data = {}
+	local items = {}
 	if queries == nil then
-		return data
+		return items
 	end
 
 	local query_obj = vim.treesitter.query.parse(filetype, queries)
-	local current_param = {}
+	local current_item = {}
 	for id, capture_node, _ in query_obj:iter_captures(node, 0) do
 		local capture_name = query_obj.captures[id]
 		local node_text = vim.treesitter.get_node_text(capture_node, 0)
 
-		if capture_name == "item_name" then
-			if current_param.name ~= nil then
-				table.insert(data, current_param)
-				current_param = {}
-				current_param.name = node_text
-			elseif not identifier_pos then
-				current_param.name = node_text
-				table.insert(data, current_param)
-				current_param = {}
-			elseif identifier_pos then
-				current_param.name = node_text
-			end
-		elseif capture_name == "item_type" then
-			if not identifier_pos then
-				current_param.type = node_text
-			else
-				current_param.type = node_text
-				table.insert(data, current_param)
-				current_param = {}
-			end
+		if identifier_pos then
+			current_item = get_parsed_item_name_first(capture_name, items, current_item, node_text)
+		else
+			current_item = get_parsed_item_name_last(capture_name, items, current_item, node_text)
 		end
 	end
--- Add the leftover param to the list
-	if next(current_param) ~= nil then
-		table.insert(data, current_param)
+	-- Add the leftover param to the list
+	if next(current_item) ~= nil then
+		table.insert(items, current_item)
 	end
 
-  	return data
+  	return items
 end
 
 local function get_node_data(node, struct_name, sections, filetype, include_type)
@@ -76,22 +88,22 @@ local function get_node_data(node, struct_name, sections, filetype, include_type
 	local node_queries = lang_data[struct_name]
 	for _, section_name in pairs(sections) do
 		local section_queries = node_queries[section_name]
-		local section_data
+		local items
 		for _, section_query in pairs(section_queries) do
 			if type(section_query) == "string" then
-				section_data = parse_node_with_identifier_first(node, include_type, filetype, section_query, identifier_pos)
+				items = get_parsed_items(node, include_type, filetype, section_query, identifier_pos)
 			else
 				local node_type = section_query[1]
 				local def_val = section_query[2]
-				section_data = search_node_recursively(node, node_type, def_val)
-				if section_data == nil then
-					section_data = {}
+				items = search_node_recursively(node, node_type, def_val)
+				if items == nil then
+					items = {}
 				end
 			end
 
-			if #section_data > 0 then break end
+			if #items > 0 then break end
 		end
-		data[section_name] = section_data
+		data[section_name] = items
 	end
 	return data
 end
