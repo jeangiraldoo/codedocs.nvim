16b793ac6f55152220bb571f759a416234824698
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2025-01-28 19:52:00 -0500

    refactor(opts): Rename individual options for improved clarity

--- lua/codedocs/docs_gen/styles/opts.lua
+++ lua/codedocs/docs_gen/styles/opts.lua
@@ -11,50 +11,37 @@ local general = {
 	section_underline = {val = "section_underline", type = "string"}, -- Character used to underline each letter in a section title
 	section_title_gap = {val = "section_title_gap", type = "boolean"}, -- Adds a gap after the title if true and there’s at least one item
 	item_gap = {val = "item_gap", type = "boolean"}, -- Adds a gap between items if true and there’s more than one item
-	section_order = {val = "section_order", type = "table"}
+	section_order = {val = "section_order", type = "table"} -- Determines the order of the docstring's sections
 }
 
 local item = {
-	title = {val = "params_title", type = "string"}, -- Title of the parameter section
-	inline = {val = "param_inline", type = "boolean"}, -- Determines if the parameter name and type appear on the same line
-	indent = {val = "param_indent", type = "boolean"}, -- Determines wether or not the parameters should be indented
-	include_type = {val = "include_param_type", type = "boolean"}, -- Determines if the parameter type is included
-	type_first = {val = "param_type_first", type = "boolean"}, -- Determines if the parameter type is placed before the name
-	kw = {val = "param_kw", type = "string"}, -- Keyword prefixed to each parameter name in the docstring
-	type_kw = {val = "param_type_kw", type = "string"}, -- Keyword prefixed to each parameter type in the docstring
-	name_wrapper = {val = "param_name_wrapper", type = "table"}, -- Strings that surround each parameter name
-	type_wrapper = {val = "param_type_wrapper", type = "table"}, -- Strings that surround each parameter type
+	title = {val = "title", type = "string"}, -- Title of the section
+	inline = {val = "inline", type = "boolean"}, -- Determines if the item name and type appear on the same line
+	indent = {val = "indent", type = "boolean"}, -- Determines wether or not the items should be indented
+	include_type = {val = "include_type", type = "boolean"}, -- Determines if the item type is included
+	type_first = {val = "type_first", type = "boolean"}, -- Determines if the item type is placed before the name
+	kw = {val = "name_kw", type = "string"}, -- Keyword prefixed to each item name in the docstring
+	type_kw = {val = "type_kw", type = "string"}, -- Keyword prefixed to each item type in the docstring
+	name_wrapper = {val = "name_wrapper", type = "table"}, -- Strings that surround each item name
+	type_wrapper = {val = "type_wrapper", type = "table"}, -- Strings that surround each item type
 	is_type_below_name_first = {val = "is_type_below_name_first", type = "boolean"}, -- TODO: Check logic and add description accordingly
 }
 
 local class_general = {
-	-- include_attrs = {val = "include_attrs", type = "boolean"}
-	include_class_body_attrs = {val = "include_attrs", type = "boolean"},
-	include_instance_attrs = {val = "include_non_constructor_attrs", type = "boolean"},
-	include_only_constructor_instance_attrs = {val = "lala", type = "boolean"}
+	include_class_body_attrs = {val = "include_class_attrs", type = "boolean"},
+	include_instance_attrs = {val = "include_instance_attrs", type = "boolean"},
+	include_only_constructor_instance_attrs = {val = "include_only_construct_instance_attrs", type = "boolean"}
 }
 
-local class_item = {
-	-- include_non_constructor_attrs = {val = "include_non_constructor_attrs", type = "boolean"}
-	-- include_instance_attrs = {val = "include_non_constructor_attrs", type = "boolean"}
-}
-
-local generic = {
+local comment = {
 	struct = {val = "structure", type = "table"}, -- Base structure of a generic docstring, with one or more parts
 	title_pos = {val = "title_pos", type = "number"}, -- Line offset of the title within the docstring, relative to its start
 	direction = {val = "direction", type = "boolean"}, -- True to place the docstring above, false for below
 }
 
--- return {
--- 	func = func,
--- 	class = class,
--- 	generic = generic
--- }
-
 return {
-	generic = generic,
+	generic = comment,
 	item = item,
-	class_item = class_item,
 	class_general = class_general,
 	general = general
 }
