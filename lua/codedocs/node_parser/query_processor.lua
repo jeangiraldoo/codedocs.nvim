82d333555d0c8167e6b580929439521f2976ac4f
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2025-01-29 22:14:15 -0500

    refactor(parse_simple_query): Eliminate side effects for clarity
    Refactors parse_simple_query to simplify flow by making called functions
    return new items or data instead of mutating the item table internally.
    This improves readability, makes logic easier to track, and simplifies
    debugging.

--- lua/codedocs/node_parser/query_processor.lua
+++ lua/codedocs/node_parser/query_processor.lua
@@ -1,54 +1,47 @@
-local function get_parsed_item_name_first(capture_name, items, current_item, node_text, include_type)
+local function get_parsed_item_name_last(capture_name, node_text, current_item, include_type)
+	if capture_name == "item_name" then
+		return {name = node_text, type = current_item.type}, {}
+	elseif capture_name == "item_type" and include_type then
+		return nil, {name = current_item.name, type = node_text}
+	else
+		return nil, current_item
+	end
+end
+
+local function get_parsed_item_name_first(capture_name, node_text, current_item, include_type)
 	if capture_name == "item_name" then
 		if current_item.name ~= nil then
-			table.insert(items, current_item)
-			current_item = {}
-			current_item.name = node_text
+			return current_item, {name = node_text}
 		else
-			current_item.name = node_text
+			return nil, {name = node_text}
 		end
 	elseif capture_name == "item_type" and include_type then
-		current_item.type = node_text
-		table.insert(items, current_item)
-		current_item = {}
+		return {name = current_item.name, type = node_text}, {}
 	end
-	return current_item
 end
 
-local function get_parsed_item_name_last(capture_name, items, current_item, node_text, include_type)
-	if capture_name == "item_name" then
-		current_item.name = node_text
-		table.insert(items, current_item)
-		current_item = {}
-	elseif capture_name == "item_type" and include_type then
-		current_item.type = node_text
-	end
-	return current_item
-end
-
-local function parse_simple_query(node, include_type, filetype, query, identifier_pos)
+local function parse_items(query_obj, include_type, node, item_processor)
 	local items = {}
-	if query == nil then
-		return items
-	end
-
-	local query_obj = vim.treesitter.query.parse(filetype, query)
 	local current_item = {}
 	for id, capture_node, _ in query_obj:iter_captures(node, 0) do
 		local capture_name = query_obj.captures[id]
 		local node_text = vim.treesitter.get_node_text(capture_node, 0)
 
-		if identifier_pos then
-			current_item = get_parsed_item_name_first(capture_name, items, current_item, node_text, include_type)
-		else
-			current_item = get_parsed_item_name_last(capture_name, items, current_item, node_text, include_type)
-		end
+		local new_item, new_current = item_processor(capture_name, node_text, current_item, include_type)
+		if new_item then table.insert(items, new_item) end
+		current_item = new_current
 	end
-	-- Add the leftover param to the list
-	if next(current_item) ~= nil then
-		table.insert(items, current_item)
-	end
-  	return items
+	if next(current_item) ~= nil then table.insert(items, current_item) end
+
+	return items
+end
+
+local function parse_simple_query(node, include_type, filetype, query, identifier_pos)
+	if query == nil then return {} end
+
+	local query_obj = vim.treesitter.query.parse(filetype, query)
+	local item_processor = (identifier_pos and get_parsed_item_name_first or get_parsed_item_name_last)
+	return parse_items(query_obj, include_type, node, item_processor)
 end
 
 return {
