c295a5640de8e1de0e06d6a59dd0d66a24c6181f
Jean Giraldo <jean.giraldoo@gmail.com>
2025-02-22 12:13:40 -0500

    docs: Add help file for :help support

---
+++ plugin/codedocs.lua
@@ -0,0 +1,14 @@
+local curr_file_path = debug.getinfo(1, "S").source
+if curr_file_path:sub(1, 1) == "@" then curr_file_path = curr_file_path:sub(2) end
+
+local plugin_dir = vim.fn.fnamemodify(curr_file_path, ":p:h")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		local doc_dir = vim.fn.fnamemodify(plugin_dir .. "/../doc", ":p")
+		if vim.fn.isdirectory(doc_dir) == 1 then
+			local load_help_tag_cmd = "helptags " .. vim.fn.fnameescape(doc_dir)
+			vim.cmd(load_help_tag_cmd)
+		end
+	end,
+})
