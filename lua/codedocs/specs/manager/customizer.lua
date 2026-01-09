f57ba21e5a9c62130e34a9ba3713d4dfac35203c
Jean Giraldo <jeann.giraldo@gmail.com>
2026-01-09 09:58:35 -0500

    refactor(specs): remove `paths` to reduce complexity

--- lua/codedocs/specs/manager/customizer.lua
+++ lua/codedocs/specs/manager/customizer.lua
@@ -1,11 +1,6 @@
-local paths = require("codedocs.specs.paths")
-local path_formats, format_path = paths.path_formats, paths.format_path
-
 local function set_default_lang_style(new_styles)
 	for lang_name, new_default_style in pairs(new_styles) do
-		local lang_data = require(format_path(path_formats.data, {
-			lang_name = lang_name,
-		}))
+		local lang_data = require("codedocs.specs.langs." .. lang_name .. ".init")
 		if not lang_data then error("There is no language called " .. lang_name .. " available in codedocs") end
 		if type(new_default_style) ~= "string" then
 			error("The value assigned as the default docstring style for " .. lang_name .. " must be a string")
@@ -27,14 +22,8 @@ end
 
 local function process_style_structs(structs, style_name, lang_name)
 	for struct_name, struct_sections in pairs(structs) do
-		local struct_path = format_path(path_formats.struct_path, {
-			lang_name = lang_name,
-			struct_name = struct_name,
-		})
-		local style_path = format_path(path_formats.style, {
-			struct_path = struct_path,
-			style_name = style_name,
-		})
+		local struct_path = "codedocs.specs.langs." .. lang_name .. "." .. struct_name
+		local style_path = struct_path .. ".styles." .. style_name
 		local struct_style = require(style_path)
 		for section_name, section_opts in pairs(struct_sections) do
 			local struct_section = struct_style[section_name]
@@ -55,7 +44,7 @@ end
 
 local function update_style(user_opts)
 	for lang_name, styles in pairs(user_opts) do
-		local success = pcall(require, format_path(path_formats.data, lang_name))
+		local success = pcall(require, "codedocs.specs.langs." .. lang_name .. ".init")
 
 		if not success then error("There is no language called " .. lang_name .. " available in codedocs") end
 		for style_name, structs in pairs(styles) do
--- lua/codedocs/specs/manager/reader.lua
+++ lua/codedocs/specs/manager/reader.lua
@@ -1,44 +1,30 @@
-local paths = require("codedocs.specs.paths")
-local path_formats, format_path = paths.path_formats, paths.format_path
 local validator = require("codedocs.specs.validators.init")
 local opts = require("codedocs.specs.langs.style_opts")
 
 local function get_lang_data(lang, struct_name)
-	local struct_path = format_path(path_formats.struct_path, {
-		lang_name = lang,
-		struct_name = struct_name,
-	})
-	local lang_data = require(format_path(path_formats.data, {
-		lang_name = lang,
-	}))
+	local struct_path = "codedocs.specs.langs." .. lang .. "." .. struct_name
+	local lang_data = require("codedocs.specs.langs." .. lang .. ".init")
 	if not validator.basic.validate(lang_data, lang) then return false end
+
 	local default_style, identifier_pos = lang_data.default_style, lang_data.identifier_pos
-	local main_style = require(format_path(path_formats.style, {
-		struct_path = struct_path,
-		style_name = default_style,
-	}))
+	local main_style = require(struct_path .. ".styles." .. default_style)
 	if not validator.style.validate(opts, main_style, struct_name) then return false end
 	return { opts, main_style, identifier_pos }
 end
 
 local function get_lang_structs(lang)
-	local lang_data = require(format_path(path_formats.data, {
-		lang_name = lang,
-	}))
+	local lang_data = require("codedocs.specs.langs." .. lang .. ".init")
 	local structs = lang_data.structs
-	if not validator.structs.validate(lang, lang_data, structs, path_formats) then return false end
+	if not validator.structs.validate(lang, lang_data, structs) then return false end
 	return structs
 end
 
 local function get_struct_tree(lang, struct_name)
-	local struct_path = format_path(path_formats.struct_path, {
-		lang_name = lang,
-		struct_name = struct_name,
-	})
-	local lang_path = format_path(path_formats.tree, {
-		struct_path = struct_path,
-	})
-	return require(lang_path)
+	local struct_path = "codedocs.specs.langs." .. lang .. "." .. struct_name
+	local lang_path = struct_path .. ".tree"
+	local tree = require(lang_path)
+	print(vim.inspect(tree))
+	return tree
 end
 
 return {
