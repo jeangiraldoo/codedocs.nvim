5f4edfb19b38e680a7c6c635750584ffd4bfdd35
Jean Giraldo <jeann.giraldo@gmail.com>
2026-01-10 14:49:12 -0500

    refactor(specs/langs): inject `opts` dependency into language specs

--- lua/codedocs/specs/_langs/python/comment/styles/Google.lua
+++ lua/codedocs/specs/_langs/python/comment/styles/Google.lua
@@ -1,11 +1,9 @@
-local opts = require("codedocs.specs._langs.style_opts")
-
-local general_opts = {
-	[opts.struct.val] = { "# " },
-	[opts.title_pos.val] = 1,
-	[opts.direction.val] = true,
-}
-
-return {
-	general = general_opts,
-}
+return function(opts)
+	return {
+		general = {
+			[opts.struct.val] = { "# " },
+			[opts.title_pos.val] = 1,
+			[opts.direction.val] = true,
+		},
+	}
+end
--- lua/codedocs/specs/_langs/python/func/styles/Numpy.lua
+++ lua/codedocs/specs/_langs/python/func/styles/Numpy.lua
@@ -1,47 +1,39 @@
-local opts = require("codedocs.specs._langs.style_opts")
--- local general = opts.general
--- local opts = opts.item
-
-local general_opts = {
-	[opts.struct.val] = { '"""', "", '"""' },
-	[opts.direction.val] = false,
-	[opts.title_pos.val] = 2,
-	[opts.title_gap.val] = true,
-	[opts.section_gap.val] = true,
-	[opts.section_underline.val] = "-",
-	[opts.section_title_gap.val] = false,
-	[opts.item_gap.val] = false,
-	[opts.section_order.val] = { "params", "return_type" },
-}
-
-local params_opts = {
-	[opts.title.val] = "Parameters",
-	[opts.inline.val] = true,
-	[opts.indent.val] = false,
-	[opts.include_type.val] = true,
-	[opts.type_first.val] = false,
-	[opts.name_kw.val] = "",
-	[opts.type_kw.val] = "",
-	[opts.name_wrapper.val] = { "", "" },
-	[opts.type_wrapper.val] = { ": ", "" },
-	[opts.is_type_below_name_first.val] = false,
-}
-
-local return_type_opts = {
-	[opts.title.val] = "Returns",
-	[opts.inline.val] = true,
-	[opts.indent.val] = false,
-	[opts.include_type.val] = true,
-	[opts.type_first.val] = false,
-	[opts.name_kw.val] = "",
-	[opts.type_kw.val] = "",
-	[opts.name_wrapper.val] = { "", "" },
-	[opts.type_wrapper.val] = { "", "" },
-	[opts.is_type_below_name_first.val] = false,
-}
-
-return {
-	general = general_opts,
-	params = params_opts,
-	return_type = return_type_opts,
-}
+return function(opts)
+	return {
+		general = {
+			[opts.struct.val] = { '"""', "", '"""' },
+			[opts.direction.val] = false,
+			[opts.title_pos.val] = 2,
+			[opts.title_gap.val] = true,
+			[opts.section_gap.val] = true,
+			[opts.section_underline.val] = "-",
+			[opts.section_title_gap.val] = false,
+			[opts.item_gap.val] = false,
+			[opts.section_order.val] = { "params", "return_type" },
+		},
+		params = {
+			[opts.title.val] = "Parameters",
+			[opts.inline.val] = true,
+			[opts.indent.val] = false,
+			[opts.include_type.val] = true,
+			[opts.type_first.val] = false,
+			[opts.name_kw.val] = "",
+			[opts.type_kw.val] = "",
+			[opts.name_wrapper.val] = { "", "" },
+			[opts.type_wrapper.val] = { ": ", "" },
+			[opts.is_type_below_name_first.val] = false,
+		},
+		return_type = {
+			[opts.title.val] = "Returns",
+			[opts.inline.val] = true,
+			[opts.indent.val] = false,
+			[opts.include_type.val] = true,
+			[opts.type_first.val] = false,
+			[opts.name_kw.val] = "",
+			[opts.type_kw.val] = "",
+			[opts.name_wrapper.val] = { "", "" },
+			[opts.type_wrapper.val] = { "", "" },
+			[opts.is_type_below_name_first.val] = false,
+		},
+	}
+end
