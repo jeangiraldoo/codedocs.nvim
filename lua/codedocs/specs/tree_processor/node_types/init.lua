c5b6b987e25241c19c3def1b805cfcb5f6fbffc8
Jean Giraldo <jeann.giraldo@gmail.com>
2026-01-18 11:34:07 -0500

    refactor(node_types): inline `get_node`

--- lua/codedocs/specs/tree_processor/node_types/init.lua
+++ lua/codedocs/specs/tree_processor/node_types/init.lua
@@ -74,7 +74,6 @@ local function node_processor(query, context)
 		return query:process(context)
 	end
 end
--- local node_processor = require("codedocs.tree_processor.processor")[1]
 
 local node_template = {}
 function node_template:new(node_type, children, data)
@@ -88,7 +87,11 @@ function node_template:new(node_type, children, data)
 	return node
 end
 
-local function get_node(name)
+local function node_constructor(node_info)
+	local node_type = node_info.type
+	local children = node_info.children
+	local data = node_info.data
+
 	local type_index = {
 		simple = "branch",
 		boolean = "branch",
@@ -98,28 +101,11 @@ local function get_node(name)
 		regex = "branch",
 		group = "branch",
 	}
-	local node_type = type_index[name]
+	local node_category = type_index[node_type]
 	local getter_data = { node_template }
 
-	if node_type == "branch" then table.insert(getter_data, node_processor) end
-	return require(current_dir .. name).get_node(getter_data)
-end
-
-local function node_constructor(node_info)
-	local node_type = node_info.type
-	local children = node_info.children
-	local data = node_info.data
-	local query_nodes = {
-		simple = get_node("simple"),
-		boolean = get_node("boolean"),
-		accumulator = get_node("accumulator"),
-		finder = get_node("finder"),
-		chain = get_node("chain"),
-		regex = get_node("regex"),
-		group = get_node("group"),
-	}
-	local node = query_nodes[node_type]:new(node_type, children, data)
-	return node
+	if node_category == "branch" then table.insert(getter_data, node_processor) end
+	return require(current_dir .. node_type).get_node(getter_data):new(node_type, children, data)
 end
 
 return node_constructor
