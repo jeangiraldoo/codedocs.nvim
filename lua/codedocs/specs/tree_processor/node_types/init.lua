152ecb030fcc2ac4c5006c8b23eb21117a98ad23
Jean Giraldo <jeann.giraldo@gmail.com>
2026-01-18 17:02:00 -0500

    refactor(node_types): simplify `parse_items` result calculation

--- lua/codedocs/specs/tree_processor/node_types/init.lua
+++ lua/codedocs/specs/tree_processor/node_types/init.lua
@@ -26,11 +26,11 @@ local function get_parsed_item_name_first(capture_name, node_text, current_item,
 	end
 end
 
-local function parse_items(query_obj, include_type, node, item_processor)
+local function parse_items(node_captures, query_capture_tags, include_type, item_processor)
 	local items = {}
 	local current_item = {}
-	for id, capture_node, _ in query_obj:iter_captures(node, 0) do
-		local capture_name = query_obj.captures[id]
+	for id, capture_node, _ in node_captures do
+		local capture_name = query_capture_tags[id]
 		local node_text = vim.treesitter.get_node_text(capture_node, 0)
 
 		local new_item, new_current = item_processor(capture_name, node_text, current_item, include_type)
@@ -42,10 +42,10 @@ local function parse_items(query_obj, include_type, node, item_processor)
 	return items
 end
 
-local function get_node_matches(query_obj, node)
+local function collect_target_capture_nodes(node_captures, query_capture_tags)
 	local nodes = {}
-	for id, capture_node, _ in query_obj:iter_captures(node, 0) do
-		local capture_name = query_obj.captures[id]
+	for id, capture_node, _ in node_captures do
+		local capture_name = query_capture_tags[id]
 		if capture_name == "target" then table.insert(nodes, capture_node) end
 	end
 	return nodes
@@ -55,13 +55,15 @@ local function parse_query(node, include_type, filetype, query, identifier_pos)
 	if query == nil then return {} end
 
 	local query_obj = vim.treesitter.query.parse(filetype, query)
-	local has_target_capture = vim.tbl_contains(query_obj.captures, "target")
+	local node_captures = query_obj:iter_captures(node, 0)
+	local query_capture_tags = query_obj.captures
+
 	local result
-	if has_target_capture then
-		result = get_node_matches(query_obj, node)
+	if vim.tbl_contains(query_capture_tags, "target") then
+		result = collect_target_capture_nodes(node_captures, query_capture_tags)
 	else
 		local item_processor = (identifier_pos and get_parsed_item_name_first or get_parsed_item_name_last)
-		result = parse_items(query_obj, include_type, node, item_processor)
+		result = parse_items(node_captures, query_capture_tags, include_type, item_processor)
 	end
 	return result
 end
