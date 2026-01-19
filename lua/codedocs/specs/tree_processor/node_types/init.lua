9313411a752bb9c37aba72eb0704b4699cbb16c1
Jean Giraldo <jeann.giraldo@gmail.com>
2026-01-19 09:40:45 -0500

    refactor(node_type): remove unnecessary `type_index` table

--- lua/codedocs/specs/tree_processor/node_types/init.lua
+++ lua/codedocs/specs/tree_processor/node_types/init.lua
@@ -1,21 +1,6 @@
 return function(node_info)
-	local node_type = node_info.type
-
-	local type_index = {
-		simple = "branch",
-		boolean = "branch",
-		accumulator = "branch",
-		finder = "leaf",
-		chain = "branch",
-		regex = "branch",
-		group = "branch",
-	}
-	local node_category = type_index[node_type]
-
 	local new_node = vim.tbl_extend("force", {}, node_info)
-	local extend_new_node = require("codedocs.specs.tree_processor.node_types." .. node_type)
-
-	if node_category == "leaf" then return extend_new_node(new_node) end
+	local extend_new_node = require("codedocs.specs.tree_processor.node_types." .. node_info.type)
 
 	return extend_new_node(new_node)
 end
