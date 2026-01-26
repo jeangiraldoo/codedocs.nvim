30de12e4df14037cd92ea26989a277873a9fbcca
Jean Giraldo <jeann.giraldo@gmail.com>
2026-01-26 13:53:28 -0500

    refactor: simplify annotation building by creating content first

--- lua/codedocs/annotation_builder.lua
+++ lua/codedocs/annotation_builder.lua
@@ -6,6 +6,22 @@ local function _handle_string(string, item_name, item_type, include_type)
 	return result
 end
 
+local function _build_line(line, item, opts)
+	local line_type = type(line)
+
+	if line_type == "string" then return _handle_string(line, item.name, item.type, opts.items.include_type) end
+
+	local final = ""
+	if line_type == "table" then
+		for idx, line_part in ipairs(line) do
+			local result = _handle_string(line_part, item.name, item.type, opts.items.include_type)
+			if idx ~= 1 and result ~= "" then final = final .. " " end
+			if result ~= "" then final = final .. result end
+		end
+	end
+	return final
+end
+
 --- Builds the raw annotation content for each section, without applying the final structure
 -- Iterates through the sections in the configured order, formats each item according to
 -- its section style, and groups the resulting lines by section name
@@ -15,96 +31,74 @@ end
 local function _build_annotation_content(item_data, style)
 	local annotation_content = {}
 
-	for _, section_name in ipairs(style.general.section_order) do
+	for section_idx, section_name in ipairs(style.general.section_order) do
 		local section_items = item_data[section_name]
 		local section_style = style[section_name]
 
 		if #section_items == 0 then goto skip_section end ---A section with no items effectively has no content
 
 		local section_content = vim.deepcopy(section_style.layout)
-		for _, item in ipairs(section_items) do
+		vim.list_extend(annotation_content, section_content)
+		for item_idx, item in ipairs(section_items) do
 			local indent = section_style.items.indent and "\t" or ""
 			for _, line in ipairs(section_style.items.template) do
-				local line_type = type(line)
+				table.insert(annotation_content, indent .. _build_line(line, item, section_style))
 
-				local final = ""
-				if line_type == "string" then
-					final = _handle_string(line, item.name, item.type, section_style.items.include_type)
-				elseif line_type == "table" then
-					for idx, line_part in ipairs(line) do
-						local result = _handle_string(line_part, item.name, item.type, section_style.items.include_type)
-						if idx ~= 1 and result ~= "" then final = final .. " " end
-						if result ~= "" then final = final .. result end
-					end
+				local should_insert_item_gap = section_style.items.insert_gap_between.enabled
+					and section_items[item_idx + 1]
+				if should_insert_item_gap then
+					table.insert(annotation_content, section_style.items.insert_gap_between.text)
 				end
-				table.insert(section_content, indent .. final)
 			end
 		end
 
-		annotation_content[section_name] = section_content
+		local should_insert_section_gap = style[section_name].gap.enabled
+			and style.general.section_order[section_idx + 1]
+		if should_insert_section_gap then table.insert(annotation_content, style[section_name].gap.text) end
+
 		::skip_section::
 	end
 	return annotation_content
 end
 
---- Formats the annotation content into the final annotation structure
--- @param sections_data table Section-formatted annotation content
--- @param style table Style configuration for all sections and general options
--- @param annotation_structure table Base annotation structure to populate
--- @return table Final formatted annotation as a flat list of lines
-local function _format_annotation_content(sections_data, style, annotation_layout)
-	local general_opts = style.general
+---Formats the annotation content into the final annotation structure
+---@param annotation_layout string[] Base annotation structure to populate
+---@param content string[] Lines that will be inserted as the annotation content
+---@param insert_at number Position to insert annotation lines into
+---@param title_opts table Title section opts
+---@return string[] annotation Final formatted annotation as a flat list of lines
+local function _format_annotation_content(annotation_layout, content, insert_at, title_opts)
+	local annotation = {}
 
-	local annotation_layout_copy = vim.deepcopy(annotation_layout)
-
-	local insertion_pos = general_opts.insert_at
-
-	---Title
-	for _, title_line in ipairs(style.title.layout) do
-		table.insert(annotation_layout_copy, insertion_pos, title_line)
-		insertion_pos = insertion_pos + 1
+	for i = 1, (insert_at - 1) do
+		-- Add all lines from the original layout that appear before the position where the content is to be inserted.
+		--
+		-- This is done to avoid inserting lines one by one at the insertion position,
+		-- since that would shift all following layout lines every time and be inefficient
+		table.insert(annotation, annotation_layout[i])
 	end
 
-	if style.title.gap.enabled and vim.tbl_count(sections_data) > 0 then
-		-- local title_gap_pos = general_opts.annotation_title.pos + 1
-		table.insert(annotation_layout_copy, insertion_pos, style.title.gap.text)
-		insertion_pos = insertion_pos + 1
+	for _, title_line in ipairs(title_opts.layout) do
+		table.insert(annotation, title_line)
 	end
 
-	local sections_order = general_opts.section_order
-	for section_idx, section_name in ipairs(sections_order) do
-		local section_content = sections_data[section_name]
+	if title_opts.gap.enabled and vim.tbl_count(content) > 0 then table.insert(annotation, title_opts.gap.text) end
 
-		if section_content == nil then goto skip_section end ---A section without content shouldn't be added to the annotation
-
-		for line_idx, line in ipairs(section_content) do
-			table.insert(annotation_layout_copy, insertion_pos, line)
-			insertion_pos = insertion_pos + 1
-
-			if style[section_name].items.insert_gap_between.enabled and section_content[line_idx + 1] then
-				table.insert(annotation_layout_copy, insertion_pos, style[section_name].items.insert_gap_between.text)
-				insertion_pos = insertion_pos + 1
-			end
-		end
-
-		if style[section_name].gap.enabled and sections_order[section_idx + 1] then
-			table.insert(annotation_layout_copy, insertion_pos, style[section_name].gap.text)
-			insertion_pos = insertion_pos + 1
-		end
-		::skip_section::
+	for _, line in ipairs(content) do
+		table.insert(annotation, line)
 	end
 
-	-- Elements are inserted at the index of the last element, shifting that element to the right. As a result,
-	-- the original last element of the base annotation structure ends up at the very end of the final annotation.
-	-- This is desired when the base structure has three parts, but becomes a leftover element when it has only two
-	-- if #annotation_layout == 2 then table.remove(annotation_layout_copy, #annotation_layout_copy) end
+	for i = insert_at, #annotation_layout do
+		-- Add all lines from the original layout that appear after the position where the content is to be inserted
+		table.insert(annotation, annotation_layout[i])
+	end
 
-	return annotation_layout_copy
+	return annotation
 end
 
 return function(style, data, annotation_structure)
 	local annotation_content = _build_annotation_content(data, style)
 	Debug_logger.log("Annotation content:", annotation_content)
 
-	return _format_annotation_content(annotation_content, style, annotation_structure)
+	return _format_annotation_content(annotation_structure, annotation_content, style.general.insert_at, style.title)
 end
