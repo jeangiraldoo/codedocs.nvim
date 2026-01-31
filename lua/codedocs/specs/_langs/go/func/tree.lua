f133ddf0797fb8a8c75cc660ccbb76f681cb49fc
Jean Giraldo <jeann.giraldo@gmail.com>
2026-01-31 07:34:05 -0500

    refactor(node_types): remove `group` node in favor of `function`
    The `group` node was created as a way to allow parameter type grouping
    in Go and it's never been used for anything else since. Taking into
    account that this could be considered an edge case and that the
    `function` node's goal is helping in such cases, type grouping in Go is
    now done with it, effectively making the `group` node redundant.

--- lua/codedocs/specs/_langs/go/func/tree.lua
+++ lua/codedocs/specs/_langs/go/func/tree.lua
@@ -1,19 +1,39 @@
 return {
 	params = {
 		{
-			type = "group",
+			type = "function",
+			callback = function(node, children, lang_name, struct_style)
+				local raw_items = children[1]:process(node, lang_name, struct_style)
+
+				local final_items = {}
+				local standby = {}
+				for _, item in ipairs(raw_items) do
+					if item.name ~= "" and item.type == nil then
+						table.insert(standby, item)
+					elseif item.name ~= "" and item.type ~= "" then
+						for _, standby_item in ipairs(standby) do
+							standby_item.type = item.type
+						end
+						vim.list_extend(final_items, standby)
+						standby = {}
+						table.insert(final_items, item)
+					end
+				end
+
+				return final_items
+			end,
 			children = {
 				{
 					type = "simple",
 					query = [[
-						(function_declaration
-							(parameter_list
-								(parameter_declaration
-									(identifier) @item_name
-									(type_identifier) @item_type
-								)
+					(function_declaration
+						(parameter_list
+							(parameter_declaration
+								(identifier) @item_name
+								(type_identifier) @item_type
 							)
 						)
+					)
 					]],
 				},
 			},
