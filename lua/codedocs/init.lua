ac715fa83fb75e7ec1f19df1690218452c2bdeae
Jean Giraldo <jeann.giraldo@gmail.com>
2026-02-20 08:35:49 -0500

    refactor(tests): use item extraction and annotation building helpers
    The `item_extraction` and `annotations_spec` would reimplement the
    necessary logic to perform their actions, which meant that said logic
    was duplicated as it was also implemented in the plugin entrypoint.
    
    This change refactors the entrypoint to offer public helpers that fully
    centralize that logic so that it can be used both by the tests and the
    entrypoint itself. Additionally, the helpers can be documented in the
    future as they can be used by users and they might find them useful.

--- lua/codedocs/init.lua
+++ lua/codedocs/init.lua
@@ -2,13 +2,13 @@ local Debug_logger = require("codedocs.utils.debug_logger")
 local docs_builder = require("codedocs.annotation_builder")
 local LangSpecs = require("codedocs.lang_specs.init")
 
-local M = {}
+local Codedocs = {}
 
 local function _apply_plugin_settings(settings)
 	if settings.debug == true then require("codedocs.utils.debug_logger").enable() end
 end
 
-function M.setup(config)
+function Codedocs.setup(config)
 	if config.settings then _apply_plugin_settings(config.settings) end
 
 	if config and config.default_styles then LangSpecs.set_default_lang_style(config.default_styles) end
@@ -16,21 +16,11 @@ function M.setup(config)
 	if config and config.styles then LangSpecs.update_style(config.styles) end
 end
 
-function M.insert_docs()
-	Debug_logger.log("Plugin triggered")
-	local lang = LangSpecs.get_buffer_lang_name()
-	Debug_logger.log("Language: " .. lang)
-	if not vim.treesitter.get_parser(0, lang, { error = false }) then
-		vim.notify("Tree-sitter parser for " .. lang .. " is not installed", vim.log.levels.ERROR)
-		return
-	end
-
-	local lang_spec = LangSpecs.new(lang)
+function Codedocs.extract_item_data(lang_name)
+	local lang_spec = LangSpecs.new(lang_name)
 	local struct_name, node = require("codedocs.struct_detector")(lang_spec:get_struct_identifiers())
-	local struct_style = lang_spec:get_struct_style(struct_name, lang_spec:get_default_style())
 
 	Debug_logger.log("Structure name: " .. struct_name)
-	Debug_logger.log("Style: ", struct_style)
 
 	local items_data, pos
 	if struct_name == "comment" then
@@ -40,8 +30,32 @@ function M.insert_docs()
 	end
 
 	Debug_logger.log("Item data: ", items_data)
+	return items_data, struct_name, pos
+end
 
-	local docs = docs_builder(struct_style, items_data, struct_style.settings.layout)
+function Codedocs.orchestrate_annotation_build(lang_data)
+	local lang_spec = LangSpecs.new(lang_data.name)
+
+	local items_data, struct_name, pos = Codedocs.extract_item_data(lang_data.name)
+	local struct_style = lang_spec:get_struct_style(struct_name, lang_data.style_name or lang_spec:get_default_style())
+
+	Debug_logger.log("Structure name: " .. struct_name)
+	Debug_logger.log("Style: ", struct_style)
+	return docs_builder(struct_style, items_data, struct_style.settings.layout), pos, struct_style
+end
+
+function Codedocs.insert_docs()
+	Debug_logger.log("Plugin triggered")
+
+	local lang = LangSpecs.get_buffer_lang_name()
+	Debug_logger.log("Language: " .. lang)
+
+	if not vim.treesitter.get_parser(0, lang, { error = false }) then
+		vim.notify("Tree-sitter parser for " .. lang .. " is not installed", vim.log.levels.ERROR)
+		return
+	end
+
+	local docs, pos, struct_style = Codedocs.orchestrate_annotation_build({ name = lang })
 	Debug_logger.log("Annotation:", docs)
 
 	require("codedocs.buf_writer")(
@@ -54,4 +68,4 @@ function M.insert_docs()
 	)
 end
 
-return M
+return Codedocs
