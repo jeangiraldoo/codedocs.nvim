3c372d3ecddf3e5f3b8fd83d6e953bd0daca59af
Jean Giraldo <jeann.giraldo@gmail.com>
2026-03-11 08:57:08 -0500

    refactor: move logic from `buf_writer` to entrypoint for simplicity

--- lua/codedocs/init.lua
+++ lua/codedocs/init.lua
@@ -8,6 +8,48 @@ local function _apply_plugin_settings(settings)
 	if settings.debug == true then require("codedocs.utils.debug_logger").enable() end
 end
 
+--- Inserts an annotation relative to a structure and moves the cursor to the annotation title
+---@param annotation_lines string[]
+---@param row number 0-based annotation-related positions
+---@param relative_position "above" | "below" | "empty_target_or_above" Position relative to the target row
+local function _write_to_buffer(annotation_lines, row, relative_position)
+	assert(type(annotation_lines) == "table", "'annotation' must be a string, got " .. type(annotation_lines))
+	assert(type(relative_position) == "string", "'relative_position' must be a string, got " .. type(relative_position))
+	assert(type(row) == "number", "'row' must be a number, got " .. type(row))
+	assert(row >= 0, "'annotation_row' must be 0 or higher, got " .. row)
+
+	local should_insert_extra_line = (
+		relative_position == "empty_target_or_above" and vim.api.nvim_get_current_line() ~= ""
+	)
+		or relative_position == "above"
+		or relative_position == "below"
+
+	local final_row_pos = relative_position == "below" and row + 1 or row
+
+	if should_insert_extra_line then vim.api.nvim_buf_set_lines(0, final_row_pos, final_row_pos, false, { "" }) end
+
+	--- Lines are inserted at the cursor position
+	vim.api.nvim_win_set_cursor(0, {
+		final_row_pos + 1, -- expects 1-based row
+		0,
+	})
+	local lines = table.concat(annotation_lines, "\n")
+	vim.snippet.expand(lines)
+end
+
+---@param ts_node TSNode Treesitter node to traverse upwards from
+---@param struct_identifiers table<string, string> Treesitter node types to check for
+---@return { name: string, node: TSNode } | nil
+local function _get_supported_struct_node_data(ts_node, struct_identifiers)
+	if not ts_node then return end
+
+	local struct_name = struct_identifiers[ts_node:type()]
+
+	if struct_name then return { name = struct_name, node = ts_node, pos = ts_node:range() } end
+
+	return _get_supported_struct_node_data(ts_node:parent(), struct_identifiers)
+end
+
 function Codedocs.setup(config)
 	if config.settings then _apply_plugin_settings(config.settings) end
 
@@ -18,34 +60,38 @@ end
 
 function Codedocs.extract_item_data(lang_name)
 	local lang_spec = LangSpecs.new(lang_name)
-	local struct_name, node = require "codedocs.struct_detector"(lang_spec:get_struct_identifiers())
 
-	Debug_logger.log("Structure name: " .. struct_name)
+	vim.treesitter.get_parser(0):parse()
+	local node_at_cursor = vim.treesitter.get_node()
 
-	local items_data, pos
-	if struct_name == "comment" then
-		items_data, pos = {}, vim.api.nvim_win_get_cursor(0)[1] - 1
-	else
-		items_data, pos = lang_spec:get_struct_items(struct_name, node), node:range()
-	end
+	local struct_data = _get_supported_struct_node_data(node_at_cursor, lang_spec:get_struct_identifiers())
+		or { name = "comment" }
+
+	if struct_data.name == "comment" then return {}, struct_data.name, vim.api.nvim_win_get_cursor(0)[1] - 1 end
+
+	local items_data, pos = lang_spec:get_struct_items(struct_data.name, struct_data.node), struct_data.pos
 
 	Debug_logger.log("Item data: ", items_data)
-	return items_data, struct_name, pos
+	return items_data, struct_data.name, pos
 end
 
 function Codedocs.orchestrate_annotation_build(lang_data)
 	local lang_spec = LangSpecs.new(lang_data.name)
 
-	local items_data, struct_name, pos = Codedocs.extract_item_data(lang_data.name)
+	local items_data, struct_name, row = Codedocs.extract_item_data(lang_data.name)
 	local struct_style = lang_spec:get_struct_style(struct_name, lang_data.style_name or lang_spec:get_default_style())
 
 	Debug_logger.log("Structure name: " .. struct_name)
 	Debug_logger.log("Style: ", struct_style)
+
 	local struct_data = {
 		should_indent = struct_style.settings.indented,
-		line_num = pos + 1,
+		line_num = row + 1,
 	}
-	return docs_builder(struct_style, items_data, struct_style.settings.layout, struct_data), pos, struct_style
+
+	local annotation_lines = docs_builder(struct_style, items_data, struct_style.settings.layout, struct_data)
+
+	return annotation_lines, row, struct_style.settings.relative_position
 end
 
 function Codedocs.insert_docs()
@@ -59,14 +105,10 @@ function Codedocs.insert_docs()
 		return
 	end
 
-	local docs, pos, struct_style = Codedocs.orchestrate_annotation_build { name = lang }
-	Debug_logger.log("Annotation:", docs)
+	local annotation_lines, row, relative_position = Codedocs.orchestrate_annotation_build { name = lang }
+	Debug_logger.log("Annotation:", annotation_lines)
 
-	require "codedocs.buf_writer"(
-		docs,
-		{ title_offset = struct_style.settings.insert_at, target = pos },
-		struct_style.settings.relative_position
-	)
+	_write_to_buffer(annotation_lines, row, relative_position)
 end
 
 return Codedocs
