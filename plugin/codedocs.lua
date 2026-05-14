5dcd74b69d9eff5ae3ff97b33f3703b2058f3e1b
Jean Giraldo <jeann.giraldo@gmail.com>
2026-05-14 12:50:28 -0500

    fix: only write `tags` file if dir is writable

--- plugin/codedocs.lua
+++ plugin/codedocs.lua
@@ -3,13 +3,12 @@ if curr_file_path:sub(1, 1) == "@" then curr_file_path = curr_file_path:sub(2) e
 
 local plugin_dir = vim.fn.fnamemodify(curr_file_path, ":p:h")
 
--- Load help tags
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		local doc_dir = vim.fn.fnamemodify(plugin_dir .. "/../doc", ":p")
-		if vim.fn.isdirectory(doc_dir) == 1 then
-			local load_help_tag_cmd = "helptags " .. vim.fn.fnameescape(doc_dir)
-			vim.cmd(load_help_tag_cmd)
+
+		if vim.fn.isdirectory(doc_dir) == 1 and vim.fn.filewritable(doc_dir) == 2 then
+			pcall(vim.cmd, "silent helptags " .. vim.fn.fnameescape(doc_dir))
 		end
 	end,
 })
