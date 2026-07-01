f125760908e9b9f72fbf9fe799fd376045cb0294
Jean Giraldo <jeann.giraldo@gmail.com>
2026-07-01 14:49:03 -0500

    fix: python default parameter items not being extracted

--- queries/python/codedocs_func_params.scm
+++ queries/python/codedocs_func_params.scm
@@ -4,5 +4,9 @@
 			(identifier) @item_name
 			(#not-eq? @item_name "self")
 			(type) @item_type)
+		(typed_default_parameter
+			(identifier) @item_name
+			(#not-eq? @item_name "self")
+			(type) @item_type)
 		(identifier) @item_name (#not-eq? @item_name "self")
 	])
